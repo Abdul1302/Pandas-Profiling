989e5006643cd73f423dc55840ec9ed736890b21
Abdul Rehman <abdul.rehman132802@gmail.com>
2024-11-18 13:10:55 +0500

    commit

--- eda.py
+++
@@ -1,6 +0,0 @@
-from nbformat import write
-import streamlit as st
-import pandas as pd
-import ydata_profiling 
-from streamlit_pandas_profiling import st_profile_report 
-from ydata_profiling import ProfileReportx
