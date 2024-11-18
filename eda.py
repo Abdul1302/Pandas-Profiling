59fce3b32d354eca65b2cf731b0cdc6eebb94a9c
Abdul Rehman <abdul.rehman132802@gmail.com>
2024-11-18 21:12:28 +0500

    commit

--- eda.py
+++ eda.py
@@ -1,18 +1,24 @@
 import streamlit as st
 import pandas as pd
 from ydata_profiling import ProfileReport
-from streamlit_  import st_profile_report 
-import sys
-import os
+from streamlit_ydata_profiling  import st_profile_report 
 
+st.title("Exploratory Data Analysis Report")
 #sidebar
-with st.sidebar:
-    uploaded_file=st.file_uploader("upload .csv, .xlsx files not exceeding 10 MB")
+with st.sidebar: 
+    uploaded_file=st.file_uploader("upload .csv, .xlsx files")
+        
+
 
 if uploaded_file is not None:
 
     #time being let load csv
     df=pd.read_csv(uploaded_file)
-
     st.dataframe(df.head())
 
+     # Generate the report with a customized dark theme
+    with st.spinner('Generating Report'):
+
+        pr=ProfileReport(df)
+
+    st_profile_report(pr)    
